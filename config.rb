09f33f98ee2efdd70d37b36f5b42971abc7cc02a
teppeis <teppeis@gmail.com>
2011-12-29 16:11:56 +0900

    Removed debug comment.

--- config.rb
+++ config.rb
@@ -13,7 +13,7 @@ output_style = :expanded
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
